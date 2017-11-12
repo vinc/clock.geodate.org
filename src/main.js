ed6dd1e4b744ae210b8da3420666a85611f09ab4
Vincent Ollivier <v@vinc.cc>
2017-11-12 14:09:26 +0100

    Display position

--- src/main.js
+++ src/main.js
@@ -3,23 +3,28 @@ var wasm = require("./main.rs");
 wasm.initialize({ noExitRuntime: true }).then(function(module) {
   var geodate = module.cwrap('geodate', 'string', ['number', 'number']);
 
-  var updateGeodateDiv = function(id) {
-    var el = document.getElementById(id);
-
+  var render = function() {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(function(position) {
         var timestamp = new Date() / 1000;
         var longitude = position.coords.longitude;
-        var res = geodate(timestamp, longitude);
+        var latitude = position.coords.latitude;
+        var parts = geodate(timestamp, longitude).split(":");
 
-        el.innerHTML = res.replace(/:/g, "<span>:</span>");
-      });
+        document.getElementById("year").innerHTML      = parts[0];
+        document.getElementById("month").innerHTML     = parts[1];
+        document.getElementById("day").innerHTML       = parts[2];
+        document.getElementById("centiday").innerHTML  = parts[3];
+        document.getElementById("dimiday").innerHTML   = parts[4];
+        document.getElementById("longitude").innerHTML = longitude.toFixed(4);
+        document.getElementById("latitude").innerHTML  = latitude.toFixed(4);
+     });
     }
   };
 
   // Update display
-  updateGeodateDiv("geodate");
-  window.setInterval(updateGeodateDiv, 8640, "geodate");
+  render();
+  window.setInterval(render, 8640);
 
   // Export function
   window.geodate = geodate;
