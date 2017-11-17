2c5d9c31dccad42ea93b175d7bed1cfbe39dee90
Vincent Ollivier <v@vinc.cc>
2017-11-17 21:43:10 +0100

    Update date format

--- src/main.js
+++ src/main.js
@@ -3,28 +3,46 @@ var wasm = require("./main.rs");
 wasm.initialize({ noExitRuntime: true }).then(function(module) {
   var geodate = module.cwrap('geodate', 'string', ['number', 'number']);
 
-  var render = function() {
+  var microday = 100;
+  var longitude;
+  var latitude;
+
+  var sync = function() {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        var timestamp = new Date() / 1000;
-        var longitude = position.coords.longitude;
-        var latitude = position.coords.latitude;
-        var parts = geodate(timestamp, longitude).split(":");
-
-        document.getElementById("year").innerHTML      = parts[0];
-        document.getElementById("month").innerHTML     = parts[1];
-        document.getElementById("day").innerHTML       = parts[2];
-        document.getElementById("centiday").innerHTML  = parts[3];
-        document.getElementById("dimiday").innerHTML   = parts[4];
-        document.getElementById("longitude").innerHTML = longitude.toFixed(4);
-        document.getElementById("latitude").innerHTML  = latitude.toFixed(4);
+        longitude = position.coords.longitude;
+        latitude = position.coords.latitude;
      });
     }
   };
 
+  var render = function() {
+    if (latitude && longitude) {
+      if (microday >= 100) {
+        microday = 0;
+        var timestamp = new Date() / 1000;
+        var parts = geodate(timestamp, longitude).split(":");
+
+        document.getElementById("century").innerHTML   = parts[0];
+        document.getElementById("year").innerHTML      = parts[1];
+        document.getElementById("month").innerHTML     = parts[2];
+        document.getElementById("day").innerHTML       = parts[3];
+        document.getElementById("centiday").innerHTML  = parts[4];
+        document.getElementById("dimiday").innerHTML   = parts[5];
+        document.getElementById("longitude").innerHTML = longitude.toFixed(4);
+        document.getElementById("latitude").innerHTML  = latitude.toFixed(4);
+      }
+
+      microday++;
+      document.getElementById("microday").innerHTML = ("00" + (microday % 100)).slice(-2);
+    }
+  };
+
   // Update display
+  sync();
   render();
-  window.setInterval(render, 8640);
+  window.setInterval(render, 86.4);
+  window.setInterval(sync, 100000);
 
   // Export function
   window.geodate = geodate;
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,27 @@ extern crate geodate;
 
 use std::os::raw::c_char;
 use std::ffi::CString;
-use geodate::geodate::*;
+use geodate::geodate::get_date;
 
 fn main() { // FIXME: remove main()
     println!("geodate loaded");
+    println!("test {}", get_date(0, 0.0));
 }
 
 #[no_mangle]
 pub fn geodate(timestamp: i32, longitude: f32) -> *mut c_char {
-    let date = get_lunisolar_date(timestamp as i64, longitude as f64);
+    let date = get_date(timestamp as i64, longitude as f64);
 
     CString::new(date).unwrap().into_raw()
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_returns_a_geodate() {
-        assert_eq!(geodate(1403322675, -1.826189), "44:05:24:15:42".into());
+        assert_eq!("00:69:11:22:99:75", get_date(0, 0.0));
+        assert_eq!("01:14:05:24:15:42", get_date(1403322675, -1.826189));
     }
 }
